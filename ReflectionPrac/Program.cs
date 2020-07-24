50a5a2511366bd395a321c49f9e6a9d245f5e350
Sagie Bar-Hame <2603sagie@gmail.com>
2020-07-24 21:58:54 +0300

    first commit

---
+++ ReflectionPrac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get all the assembly information about the current saamebly
+            var assembly = Assembly.GetExecutingAssembly();
+        }
+    }
+}
